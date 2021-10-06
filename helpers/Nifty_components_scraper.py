b86d692ad5eae0d1eea0c8f90c3f2a73cad57d73
Pratik Choudhari <40862682+pratik-choudhari@users.noreply.github.com>
2021-10-06 11:14:56 +0530

    Update Nifty_components_scraper.py

--- helpers/Nifty_components_scraper.py
+++ helpers/Nifty_components_scraper.py
@@ -14,14 +14,14 @@ driver.get("https://www1.nseindia.com/live_market/dynaContent/live_watch/equitie
 def nifty50():
     tbody = driver.find_element_by_xpath('//*[@id="dataTable"]/tbody')
     rows = tbody.find_elements_by_tag_name("tr")
-    nifty_components = {"stocks": []}
+    nifty_stocks = {"stocks": []}
     for row in rows:
         try:
-            nifty_components["stocks"].append(row.find_element_by_tag_name("td").find_element_by_tag_name("a").text)
+            nifty_stocks["stocks"].append(row.find_element_by_tag_name("td").find_element_by_tag_name("a").text)
         except selenium.common.exceptions.NoSuchElementException as e:
             print("Element not found")
     driver.close()
-    return nifty_components
+    return nifty_stocks
 
 
 def niftynext50(nifty50_components):
